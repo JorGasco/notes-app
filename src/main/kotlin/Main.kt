dbf777eb6b19fade7eb2806e5985dd268715ee1e
Jorge <20099103@mail.wit.ie>
2023-03-15 03:19:43 +0000

    adding a Menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,19 @@
 val scanner = Scanner(System.`in`)
 
+fun mainMenu() : Int {
+    println("")
+    println("--------------------")
+    println("NOTE KEEPER APP")
+    println("--------------------")
+    println("NOTE MENU")
+    println("  1) Add a note")
+    println("  2) List all notes")
+    println("  3) Update a note")
+    println("  4) Delete a note")
+    println("--------------------")
+    println("  0) Exit")
+    println("--------------------")
+    print("==>> ")
+    return scanner.nextInt()
+}
+
